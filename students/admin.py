5900d8ce79c68f508e7d860744366f94647892e2
Rowena <an.perevalova04@gmail.com>
2019-01-16 18:29:07 +0200

    upgrade students & groups list in admin part

--- students/admin.py
+++ students/admin.py
@@ -1,7 +1,32 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 from .models import Student, Group, Exam
+from django.core.urlresolvers import reverse
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'ticket', 'student_group']
+    list_display_links = ['last_name', 'first_name']
+    list_editable = ['student_group']
+    ordering = ['last_name']
+    list_filter = ['student_group']
+    list_per_page = 10
+    search_fields = ['last_name', 'first_name', 'middle_name', 'ticket', 'notes']
+
+    def view_on_site(self, obj):
+        return reverse('students_edit', kwargs={'pk': obj.id})
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title', 'leader']
+    list_display_links = ['title', 'leader']
+    ordering = ['title']
+    list_per_page = 10
+    search_fields =  ['title', 'leader', 'notes']
+
+    def view_on_site(self, obj):
+        return reverse('groups_edit', kwargs={'pk': obj.id})
 
 # Register your models here.
-admin.site.register(Student)
-admin.site.register(Group)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Exam)
