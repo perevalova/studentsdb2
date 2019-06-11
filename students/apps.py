ec689e679869d704d7cdde80b9d2cadcbe95490f
Anastasia Perevalova <perev4n@gmail.com>
2019-06-11 18:40:17 +0300

    logging students

--- students/apps.py
+++ students/apps.py
@@ -1,5 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.apps import AppConfig
 
 
-class StudentsConfig(AppConfig):
+class StudentsAppConfig(AppConfig):
     name = 'students'
+    verbose_name = u'База Студентів'
+
+    def ready(self):
+        from students import signals
