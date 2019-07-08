31fb9f2c27fe78f2046c9bd86cf11143ecffbbed
Anastasia Perevalova <perev4n@gmail.com>
2019-07-08 10:50:44 +0300

    cancel localization

--- students/management/commands/stcount.py
+++ students/management/commands/stcount.py
@@ -12,13 +12,13 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         # Positional arguments
-        parser.add_argument(_('student'), help='<model_name model_name ...>')
-        parser.add_argument(_('group'), help='<model_name model_name ...>')
-        parser.add_argument(_('user'), help='<model_name model_name ...>')
+        parser.add_argument('student', help='<model_name model_name ...>')
+        parser.add_argument('group', help='<model_name model_name ...>')
+        parser.add_argument('user', help='<model_name model_name ...>')
 
     def handle(self, *args, **options):
         for name, model in self.models:
             if name in options:
-                self.stdout.write(_('Number of %(var)ss in database: %(modelname)d', dict(var=name, modelname=model.objects.count())))
+                self.stdout.write('Number of %ss in database: %d' % (name, model.objects.count()))
             else:
                 raise CommandError()
