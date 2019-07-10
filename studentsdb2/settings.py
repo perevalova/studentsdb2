d17b513c014ef25658d3112f0d8e63d0c263261a
Anastasia Perevalova <perev4n@gmail.com>
2019-07-10 21:06:25 +0300

    fixed bugs

--- studentsdb2/settings.py
+++ studentsdb2/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
+import django_heroku
 import os
 
 from decouple import config
@@ -262,3 +263,6 @@ DATABASES['default'].update(db_from_env)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
