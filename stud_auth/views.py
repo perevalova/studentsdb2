c7802c6c48acd82b5179c323b64b882c79e81fa9
Anastasia Perevalova <perev4n@gmail.com>
2019-07-22 17:45:03 +0300

    fixed user detail

--- stud_auth/views.py
+++ stud_auth/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import DetailView
 from django.views.generic.base import TemplateView
 
 from django.contrib.auth.models import User
@@ -5,29 +6,12 @@ from django.contrib.auth.models import User
 from students.util import paginate
 
 
-class UserView(TemplateView):
+class UserView(DetailView):
     """docstring for UserView
     This view takes User's id of url request and return User
     """
     template_name = 'registration/user.html'
-
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        users_obj = User.objects.get(pk=kwargs['pk'])
-        context['username'] = users_obj.username
-        context['full_name'] = users_obj.get_full_name
-        context['email'] = users_obj.email
-        context['date_joined'] = users_obj.date_joined.strftime('%d.%m.%Y')
-        try:
-            context['photo'] = users_obj.stprofile.photo
-            context['mobile_phone'] = users_obj.stprofile.mobile_phone
-            context['passport'] = users_obj.stprofile.passport
-        except Exception:
-            context['photo'] = ''
-            context['passport'] = ''
-            context['mobile_phone'] = ''
-
-        return self.render_to_response(context)
+    model = User
 
 
 class UserList(TemplateView):
