2cf2c1c5422cf74c09d4d1d417c0f26827e3ec24
Anastasia Perevalova <perev4n@gmail.com>
2020-01-09 22:49:07 +0200

    update urls

--- students/urls.py
+++ students/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
+from django.urls import path
+
 from students.views.students import StudentList, StudentUpdateView, StudentAddView, StudentDeleteView
 from students.views.groups import GroupList, GroupAddView, GroupUpdateView, GroupDeleteView
 from students.views.exams import ExamList, ExamAddView, ExamUpdateView, ExamDeleteView
@@ -7,23 +9,24 @@ from students.views.journal import JournalView
 
 
 urlpatterns = [
-    url(r'^$', StudentList.as_view(), name='home'),
-    url(r'^students/add/$', login_required(StudentAddView.as_view()), name='students_add'),
-    url(r'^students/(?P<pk>\d+)/edit/$', login_required(StudentUpdateView.as_view()), name='students_edit'),
-    url(r'^students/(?P<pk>\d+)/delete/$', login_required(StudentDeleteView.as_view()), name='students_delete'),
+    path('', StudentList.as_view(), name='home'),
+    path('students/add/', login_required(StudentAddView.as_view()), name='students_add'),
+    path('students/<int:pk>/edit/', login_required(StudentUpdateView.as_view()), name='students_edit'),
+    path('students/<int:pk>/delete/', login_required(StudentDeleteView.as_view()), name='students_delete'),
 
     # Groups urls
-    url(r'^groups/$', login_required(GroupList.as_view()), name='groups'),
-    url(r'^groups/add/$', login_required(GroupAddView.as_view()), name='groups_add'),
-    url(r'^groups/(?P<pk>\d+)/edit/$', login_required(GroupUpdateView.as_view()), name='groups_edit'),
-    url(r'^groups/(?P<pk>\d+)/delete/$', login_required(GroupDeleteView.as_view()), name='groups_delete'),
+    path('groups/', login_required(GroupList.as_view()), name='groups'),
+    path('groups/add/', login_required(GroupAddView.as_view()), name='groups_add'),
+    path('groups/<int:pk>/edit/', login_required(GroupUpdateView.as_view()), name='groups_edit'),
+    path('groups/<int:pk>/delete/', login_required(GroupDeleteView.as_view()), name='groups_delete'),
 
     # Visiting urls
+    # path('journal/<int:pk>/', login_required(JournalView.as_view()), name='journal'),
     url(r'^journal/(?P<pk>\d+)?/?$', login_required(JournalView.as_view()), name='journal'),
 
     # Exams urls
-    url(r'^exams/$', login_required(ExamList.as_view()), name='exams'),
-    url(r'^exams/add/$', login_required(ExamAddView.as_view()), name='exams_add'),
-    url(r'^exams/(?P<pk>\d+)/edit/$', login_required(ExamUpdateView.as_view()), name='exams_edit'),
-    url(r'^exams/(?P<pk>\d+)/delete/$', login_required(ExamDeleteView.as_view()), name='exams_delete'),
+    path('exams/', login_required(ExamList.as_view()), name='exams'),
+    path('exams/add/', login_required(ExamAddView.as_view()), name='exams_add'),
+    path('exams/<int:pk>/edit/', login_required(ExamUpdateView.as_view()), name='exams_edit'),
+    path('exams/<int:pk>/delete/', login_required(ExamDeleteView.as_view()), name='exams_delete'),
 ]
