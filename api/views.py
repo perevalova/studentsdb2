a5ae9e1c1023165f990a08e034a92473f5896010
Anastasia Perevalova <perev4n@gmail.com>
2020-01-17 20:06:39 +0200

    add searching and filtering

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, filters
 from students.models import Student, Group, Exam, MonthJournal
 from .serializers import StudentSerializer, GroupSerializer, ExamSerializer, \
     MonthJournalSerializer, ContactAdminSerializer
@@ -7,21 +7,33 @@ from .serializers import StudentSerializer, GroupSerializer, ExamSerializer, \
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['first_name', 'last_name']
+    ordering_fields = ['last_name', 'student_group__title']
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
 
 
 class JournalViewSet(viewsets.ModelViewSet):
     queryset = MonthJournal.objects.all()
     serializer_class = MonthJournalSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['student__last_name']
+    ordering_fields = ['date', 'student__last_name']
+    ordering = ['student__last_name']
 
 
 class ExamViewSet(viewsets.ModelViewSet):
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['subject', 'exam_group__title']
+    ordering_fields = ['date', 'subject']
 
 
 class ContactView(generics.CreateAPIView):
